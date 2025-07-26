2bf1a5c7f09c45878ce740752390a242452002c3
Skhshshs <hwjsbebwbwhj@gmail.com>
2025-07-26 19:24:24 +0700

    Update node.js

--- node.js
+++ node.js
@@ -10,7 +10,7 @@ const CHAT_ID = '6790410023'
 const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}`
 const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1376391242576957562/2cmM6ySlCSlbSvYMIn_jVQ6zZLGH6OLx5LLhuzDNh4mxFdHNQSqgRnKcaNvilZ-m8HSe'
 const PIN = '0301'
-const USE_PROXY = false
+const USE_PROXY = true
 const PROXY_URL = 'socks5://96.126.96.163:9090'
 const agent = USE_PROXY ? new SocksProxyAgent(PROXY_URL) : undefined
 
